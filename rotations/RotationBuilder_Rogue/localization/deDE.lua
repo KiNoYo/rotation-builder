9de56458b83ecca545fa39e83217c044ef58a911
Vianney Dubus <vianney.dubus@gmail.com>
2015-08-27 17:06:22 +0000

    Add sub-addon localizations for each language.
    git-svn-id: https://repos.curseforge.com/wow/rotation-builder/trunk@75 3bc1ffbe-e05c-434b-a64b-d7d832f83c92

---
+++ rotations/RotationBuilder_Rogue/localization/deDE.lua
@@ -0,0 +1,4 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("RotationBuilder", "deDE")
+if not L then return end
+
+--@localization(locale="deDE", format="lua_additive_table", namespace="RotationBuilder_Rogue")@
