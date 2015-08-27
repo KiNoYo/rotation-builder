b44c1503df5721646aa499983d85802d3f39fbbc
Vianney Dubus <vianney.dubus@gmail.com>
2015-08-27 14:05:56 +0000

    Clean up of dead code.
    git-svn-id: https://repos.curseforge.com/wow/rotation-builder/trunk@73 3bc1ffbe-e05c-434b-a64b-d7d832f83c92

--- debug.lua
+++ debug.lua
@@ -2,9 +2,6 @@ ROB_ROTATION_STATE = 0
 ROB_DEBUG_STATE    = 1
  
 function ROB_ToggleDebugState()
-   --if (ROB_SelectedRotationName == nil) then
-   --   print("No rotation selected. Select a rotation first.")
-   --end
    if (ROB_ROTATION_STATE == 0) then
       if (ROB_DEBUG_STATE==1) then print("DEBUG ON") end;
       ROB_ROTATION_STATE = 1
