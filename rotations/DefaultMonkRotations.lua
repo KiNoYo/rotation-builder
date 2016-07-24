25d132984aed20e7118079d10018ff7e600d60cc
Jocelyn Dubus <jocelyn.dubus@laposte.net>
2016-07-24 17:05:38 +0000

    - Basic update to monk rotations for patch 7.0.3
    git-svn-id: https://repos.curseforge.com/wow/rotation-builder/branches/id-fix@123 3bc1ffbe-e05c-434b-a64b-d7d832f83c92

--- rotations/DefaultMonkRotations.lua
+++ rotations/DefaultMonkRotations.lua
@@ -1,397 +1,165 @@
 -- The default rotations generator for MONKS.
 local monkRotationsGenerator = function ()
 	local rotation = {
-		["defaultRotation/monk/windwalker"] = {
+		["defaultRotation/monk/mistweaver"] = {
 			["version"] = 1,
-			["specID"] = 3,
+			["keybind"] = "<keybind>",
+			["specID"] = 2,
+			["SortedActions"] = {
+				"defaultRotation/monk/rising_sun_kick", -- [1]
+				"defaultRotation/monk/blackout_kick", -- [2]
+				"defaultRotation/monk/tiger_palm", -- [3]
+			},
 			["ActionList"] = {
-				["defaultRotation/monk/energizing_brew"] = {
-					["v_gunitpowertype"] = "3",
-					["v_gunitpower"] = "60",
-					["v_p_unitpowertype"] = "3",
-					["b_gunitpower"] = true,
-					["v_togglename"] = "Toggle 1",
-					["b_p_unitpower"] = true,
-					["v_spellname"] = "115288",
-					["v_actionicon"] = "115288",
-					["v_keybind"] = "<keybind>",
-					["v_p_unitpower"] = "<20",
-					["v_durationstartedtime"] = 0,
-				},
-				["defaultRotation/monk/expel_harm"] = {
-					["v_gunitpowertype"] = "12",
-					["v_p_hp"] = "<80%",
-					["b_p_hp"] = true,
-					["v_durationstartedtime"] = 0,
-					["v_togglename"] = "Toggle 1",
-					["b_gunitpower"] = true,
-					["v_spellname"] = "115072",
-					["v_actionicon"] = "115072",
-					["v_keybind"] = "<keybind>",
-					["v_gunitpower"] = "1",
-				},
-				["defaultRotation/monk/tigereye_brew"] = {
-					["v_durationstartedtime"] = 0,
-					["v_togglename"] = "Toggle 1",
-					["v_actionicon"] = "116740",
-					["v_spellname"] = "116740",
-					["b_p_havebuff"] = true,
-					["v_keybind"] = "<keybind>",
-					["v_p_havebuff"] = "125195#10",
-				},
-				["defaultRotation/monk/chi_explosion"] = {
-					["v_p_knowspell"] = "157676",
-					["v_actionicon"] = "157676",
-					["b_p_unitpower"] = true,
-					["b_p_knowspell"] = true,
-					["v_togglename"] = "Toggle 1",
-					["v_durationstartedtime"] = 0,
-					["v_spellname"] = "100784",
-					["v_p_unitpowertype"] = "12",
-					["v_keybind"] = "<keybind>",
-					["v_p_unitpower"] = ">=3",
-				},
-				["defaultRotation/monk/legacy_of_the_white_tiger"] = {
-					["v_p_needbuff"] = "1126|115921|116781|20217|160206|159988|160017|90363|160077",
-					["v_togglename"] = "Toggle 1",
-					["v_durationstartedtime"] = 0,
-					["v_spellname"] = "116781",
-					["b_p_needbuff"] = true,
-					["v_keybind"] = "<keybind>",
-					["v_actionicon"] = "116781",
-				},
-				["defaultRotation/monk/fists_of_fury"] = {
-					["v_p_unitpowertype"] = "12",
-					["v_actionicon"] = "113656",
-					["v_togglename"] = "Toggle 1",
-					["v_durationstartedtime"] = 0,
-					["v_spellname"] = "113656",
-					["b_p_unitpower"] = true,
-					["v_keybind"] = "<keybind>",
-					["v_p_unitpower"] = ">=3",
-				},
-				["defaultRotation/monk/flying_serpent_kick_landing"] = {
-					["b_p_havebuff"] = true,
-					["v_togglename"] = "Toggle 1",
-					["v_actionicon"] = "101454",
-					["v_spellname"] = "101545",
-					["v_durationstartedtime"] = 0,
-					["v_keybind"] = "<keybind>",
-					["v_p_havebuff"] = "115057",
-				},
-				["defaultRotation/monk/stance_of_the_fierce_tiger"] = {
-					["v_p_notstance"] = "2",
-					["v_togglename"] = "Toggle 1",
-					["v_durationstartedtime"] = 0,
-					["v_spellname"] = "103985",
-					["b_p_notstance"] = true,
-					["v_keybind"] = "<keybind>",
-					["v_actionicon"] = "103985",
-				},
-				["defaultRotation/monk/blackout_kick_combo_breaker"] = {
-					["v_p_knownotspell"] = "157676",
-					["b_p_knownotspell"] = true,
-					["v_durationstartedtime"] = 0,
-					["v_togglename"] = "Toggle 1",
-					["b_p_havebuff"] = true,
-					["v_spellname"] = "100784",
-					["v_actionicon"] = "100784",
-					["v_keybind"] = "<keybind>",
-					["v_p_havebuff"] = "137384",
-				},
 				["defaultRotation/monk/rising_sun_kick"] = {
-					["v_p_needdebuff"] = "_185099^3",
-					["b_p_needdebuff"] = true,
-					["v_actionicon"] = "107428",
-					["v_p_unitpowertype"] = "12",
 					["v_togglename"] = "Toggle 1",
-					["b_p_unitpower"] = true,
+					["v_keybind"] = "<keybind>",
+					["v_durationstartedtime"] = 0,
 					["v_spellname"] = "107428",
-					["v_durationstartedtime"] = 0,
-					["v_keybind"] = "<keybind>",
-					["v_p_unitpower"] = ">=2",
-				},
-				["defaultRotation/monk/chi_wave"] = {
-					["v_p_knowspell"] = "115098",
-					["b_p_knowspell"] = true,
-					["v_togglename"] = "Toggle 1",
-					["v_durationstartedtime"] = 0,
-					["v_spellname"] = "115098",
-					["v_keybind"] = "<keybind>",
-					["v_actionicon"] = "115098",
-				},
-				["defaultRotation/monk/flying_serpent_kick"] = {
-					["v_togglename"] = "Toggle 1",
-					["v_durationstartedtime"] = 0,
-					["v_spellname"] = "101545",
-					["v_actionicon"] = "101454",
-					["v_keybind"] = "<keybind>",
-				},
-				["defaultRotation/monk/tiger_palm_combo_breaker"] = {
-					["v_durationstartedtime"] = 0,
-					["v_togglename"] = "Toggle 1",
-					["b_p_havebuff"] = true,
-					["v_spellname"] = "100787",
-					["v_actionicon"] = "100787",
-					["v_keybind"] = "<keybind>",
-					["v_p_havebuff"] = "137384",
-				},
-				["defaultRotation/monk/touch_of_death"] = {
-					["v_actionicon"] = "115080",
-					["v_durationstartedtime"] = 0,
-					["v_togglename"] = "Toggle 1",
-					["b_p_unitpower"] = true,
-					["v_spellname"] = "115080",
-					["v_p_unitpowertype"] = "12",
-					["v_keybind"] = "<keybind>",
-					["v_p_unitpower"] = ">=3",
-				},
-				["defaultRotation/monk/chi_explosion_combo_breaker"] = {
-					["v_actionicon"] = "157676",
-					["b_p_havebuff"] = true,
-					["v_p_knowspell"] = "157676",
-					["v_p_havebuff"] = "137384",
-					["b_p_unitpower"] = true,
-					["b_p_knowspell"] = true,
-					["v_togglename"] = "Toggle 1",
-					["v_durationstartedtime"] = 0,
-					["v_spellname"] = "100784",
-					["v_p_unitpowertype"] = "12",
-					["v_keybind"] = "<keybind>",
-					["v_p_unitpower"] = ">=4",
-				},
-				["defaultRotation/monk/blackout_kick"] = {
-					["v_p_unitpowertype"] = "12",
-					["v_keybind"] = "<keybind>",
-					["v_togglename"] = "Toggle 1",
-					["b_p_unitpower"] = true,
-					["v_p_knownotspell"] = "157676",
-					["v_durationstartedtime"] = 0,
-					["v_spellname"] = "100784",
-					["v_actionicon"] = "100784",
-					["b_p_knownotspell"] = true,
-					["v_p_unitpower"] = ">=3",
-				},
-				["defaultRotation/monk/chi_brew"] = {
-					["v_p_knowspell"] = "115399",
-					["v_gunitpowertype"] = "12",
-					["v_spellname"] = "115399",
-					["b_charges"] = true,
-					["b_p_knowspell"] = true,
-					["v_togglename"] = "Toggle 1",
-					["b_gunitpower"] = true,
-					["v_charges"] = ">=1",
-					["v_actionicon"] = "115399",
-					["v_keybind"] = "<keybind>",
-					["v_gunitpower"] = "2",
-					["v_durationstartedtime"] = 0,
-				},
-				["defaultRotation/monk/jab"] = {
-					["v_gunitpowertype"] = "12",
-					["v_actionicon"] = "100780",
-					["v_togglename"] = "Toggle 1",
-					["v_durationstartedtime"] = 0,
-					["v_spellname"] = "100780",
-					["b_gunitpower"] = true,
-					["v_keybind"] = "<keybind>",
-					["v_gunitpower"] = "1",
-				},
-				["defaultRotation/monk/zen_sphere"] = {
-					["v_p_knowspell"] = "124081",
-					["b_p_knowspell"] = true,
-					["v_togglename"] = "Toggle 1",
-					["v_durationstartedtime"] = 0,
-					["v_spellname"] = "124081",
-					["v_keybind"] = "<keybind>",
-					["v_actionicon"] = "124081",
-				},
-				["defaultRotation/monk/invoke_xuen_the_white_tiger"] = {
-					["v_togglename"] = "Toggle 1",
-					["v_actionicon"] = "123904",
-					["v_spellname"] = "123904",
-					["v_durationstartedtime"] = 0,
-					["v_keybind"] = "<keybind>",
 				},
 				["defaultRotation/monk/tiger_palm"] = {
-					["v_p_needbuff"] = "125359^3",
-					["b_p_needbuff"] = true,
-					["v_actionicon"] = "100787",
+					["v_togglename"] = "Toggle 1",
+					["v_keybind"] = "<keybind>",
+					["v_durationstartedtime"] = 0,
+					["v_spellname"] = "100780",
+				},
+				["defaultRotation/monk/blackout_kick"] = {
+					["v_togglename"] = "Toggle 1",
+					["v_keybind"] = "<keybind>",
+					["v_durationstartedtime"] = 0,
+					["v_spellname"] = "100784",
+				},
+			},
+		},
+		["defaultRotation/monk/windwalker"] = {
+			["version"] = 1,
+			["keybind"] = "<keybind>",
+			["specID"] = 3,
+			["SortedActions"] = {
+				"defaultRotation/monk/fists_of_fury", -- [1]
+				"defaultRotation/monk/tiger_palm_energy", -- [2]
+				"defaultRotation/monk/rising_sun_kick", -- [3]
+				"defaultRotation/monk/blackout_kick_blackout_kick", -- [4]
+				"defaultRotation/monk/blackout_kick", -- [5]
+				"defaultRotation/monk/tiger_palm", -- [6]
+			},
+			["ActionList"] = {
+				["defaultRotation/monk/blackout_kick_blackout_kick"] = {
+					["v_togglename"] = "Toggle 1",
+					["v_durationstartedtime"] = 0,
+					["v_spellname"] = "100784",
+					["v_keybind"] = "<keybind>",
+					["b_hasproc"] = true,
+				},
+				["defaultRotation/monk/blackout_kick"] = {
+					["v_togglename"] = "Toggle 1",
+					["v_durationstartedtime"] = 0,
+					["v_spellname"] = "100784",
+					["b_p_unitpower"] = true,
+					["v_keybind"] = "<keybind>",
+					["v_p_unitpower"] = ">=3",
+					["v_p_unitpowertype"] = "12",
+				},
+				["defaultRotation/monk/fists_of_fury"] = {
+					["b_gspellcost"] = true,
+					["v_durationstartedtime"] = 0,
+					["b_p_unitpower"] = true,
+					["v_togglename"] = "Toggle 1",
+					["v_p_unitpowertype"] = "12",
+					["v_spellname"] = "113656",
+					["v_gspellcost"] = "3",
+					["v_keybind"] = "<keybind>",
+					["v_p_unitpower"] = ">=3",
+					["v_gspellcosttype"] = "12",
+				},
+				["defaultRotation/monk/rising_sun_kick"] = {
+					["b_gspellcost"] = true,
+					["v_gspellcost"] = "2",
 					["v_p_unitpowertype"] = "12",
 					["v_togglename"] = "Toggle 1",
 					["v_durationstartedtime"] = 0,
-					["v_spellname"] = "100787",
-					["b_p_unitpower"] = true,
+					["v_spellname"] = "107428",
+					["v_gspellcosttype"] = "12",
 					["v_keybind"] = "<keybind>",
-					["v_p_unitpower"] = ">=2",
+					["v_p_unitpower"] = ">=3",
+					["b_p_unitpower"] = true,
+				},
+				["defaultRotation/monk/tiger_palm"] = {
+					["b_gspellcost"] = true,
+					["v_gunitpowertype"] = "12",
+					["v_durationstartedtime"] = 0,
+					["v_togglename"] = "Toggle 1",
+					["b_gunitpower"] = true,
+					["v_spellname"] = "100780",
+					["v_gspellcost"] = "50",
+					["v_keybind"] = "<keybind>",
+					["v_gunitpower"] = "2",
+					["v_gspellcosttype"] = "3",
+				},
+				["defaultRotation/monk/tiger_palm_energy"] = {
+					["b_gspellcost"] = true,
+					["v_gunitpowertype"] = "12",
+					["b_gunitpower"] = true,
+					["v_p_unitpowertype"] = "3",
+					["v_gspellcosttype"] = "3",
+					["v_durationstartedtime"] = 0,
+					["v_togglename"] = "Toggle 1",
+					["b_p_unitpower"] = true,
+					["v_spellname"] = "100780",
+					["v_gspellcost"] = "50",
+					["v_keybind"] = "<keybind>",
+					["v_p_unitpower"] = ">90",
+					["v_gunitpower"] = "2",
 				},
 			},
-			["SortedActions"] = {
-				"defaultRotation/monk/stance_of_the_fierce_tiger", -- [1]
-				"defaultRotation/monk/legacy_of_the_white_tiger", -- [2]
-				"defaultRotation/monk/flying_serpent_kick_landing", -- [3]
-				"defaultRotation/monk/invoke_xuen_the_white_tiger", -- [4]
-				"defaultRotation/monk/touch_of_death", -- [5]
-				"defaultRotation/monk/tiger_palm", -- [6]
-				"defaultRotation/monk/rising_sun_kick", -- [7]
-				"defaultRotation/monk/fists_of_fury", -- [8]
-				"defaultRotation/monk/tiger_palm_combo_breaker", -- [9]
-				"defaultRotation/monk/blackout_kick_combo_breaker", -- [10]
-				"defaultRotation/monk/chi_explosion_combo_breaker", -- [11]
-				"defaultRotation/monk/energizing_brew", -- [12]
-				"defaultRotation/monk/tigereye_brew", -- [13]
-				"defaultRotation/monk/zen_sphere", -- [14]
-				"defaultRotation/monk/chi_wave", -- [15]
-				"defaultRotation/monk/blackout_kick", -- [16]
-				"defaultRotation/monk/chi_brew", -- [17]
-				"defaultRotation/monk/expel_harm", -- [18]
-				"defaultRotation/monk/jab", -- [19]
-				"defaultRotation/monk/chi_explosion", -- [20]
-				"defaultRotation/monk/flying_serpent_kick", -- [21]
-			},
-			["keybind"] = "<keybind>",
 		},
 		["defaultRotation/monk/brewmaster"] = {
 			["version"] = 1,
+			["keybind"] = "<keybind>",
 			["specID"] = 1,
 			["ActionList"] = {
-				["defaultRotation/monk/legacy_of_the_white_tiger"] = {
-					["v_p_needbuff"] = "1126|115921|116781|20217|160206|159988|160017|90363|160077",
-					["b_p_needbuff"] = true,
+				["defaultRotation/monk/blackout_strike"] = {
 					["v_togglename"] = "Toggle 1",
-					["v_durationstartedtime"] = 0,
-					["v_spellname"] = "116781",
 					["v_keybind"] = "<keybind>",
-					["v_actionicon"] = "116781",
+					["v_durationstartedtime"] = 0,
+					["v_spellname"] = "205523",
 				},
-				["defaultRotation/monk/expel_harm"] = {
-					["v_gunitpowertype"] = "12",
-					["b_gunitpower"] = true,
-					["v_keybind"] = "<keybind>",
-					["v_actionicon"] = "115072",
+				["defaultRotation/monk/breath_of_fire"] = {
 					["v_togglename"] = "Toggle 1",
 					["v_durationstartedtime"] = 0,
-					["v_spellname"] = "115072",
-					["v_p_hp"] = "<90%",
-					["b_p_hp"] = true,
-					["v_gunitpower"] = "1",
-				},
-				["defaultRotation/monk/zen_sphere"] = {
-					["v_p_knowspell"] = "124081",
-					["b_p_knowspell"] = true,
-					["v_togglename"] = "Toggle 1",
-					["v_durationstartedtime"] = 0,
-					["v_spellname"] = "124081",
+					["b_t_hasdebuff"] = true,
 					["v_keybind"] = "<keybind>",
-					["v_actionicon"] = "124081",
-				},
-				["defaultRotation/monk/summon_black_ox_statue"] = {
-					["v_p_needbuff"] = "_126119",
-					["b_p_needbuff"] = true,
-					["v_togglename"] = "Toggle 1",
-					["v_durationstartedtime"] = 0,
-					["v_spellname"] = "115315",
-					["v_keybind"] = "<keybind>",
-					["v_actionicon"] = "115315",
+					["v_spellname"] = "115181",
+					["v_t_hasdebuff"] = "_121253",
 				},
 				["defaultRotation/monk/tiger_palm"] = {
-					["v_togglename"] = "Toggle 1",
-					["v_actionicon"] = "100787",
-					["v_spellname"] = "100787",
-					["v_durationstartedtime"] = 0,
-					["v_keybind"] = "<keybind>",
-				},
-				["defaultRotation/monk/guard"] = {
-					["v_p_needbuff"] = "115295",
-					["b_p_needbuff"] = true,
-					["v_durationstartedtime"] = 0,
-					["v_togglename"] = "Toggle 1",
+					["b_gspellcost"] = true,
+					["v_p_unitpowertype"] = "3",
 					["b_p_unitpower"] = true,
-					["v_spellname"] = "115295",
-					["v_actionicon"] = "115295",
-					["v_keybind"] = "<keybind>",
-					["v_p_unitpower"] = ">=2",
-					["v_p_unitpowertype"] = "12",
-				},
-				["defaultRotation/monk/stance_of_the_sturdy_ox"] = {
-					["v_p_notstance"] = "1",
 					["v_togglename"] = "Toggle 1",
 					["v_durationstartedtime"] = 0,
-					["v_spellname"] = "115069",
-					["b_p_notstance"] = true,
+					["v_spellname"] = "100780",
+					["v_gspellcosttype"] = "3",
 					["v_keybind"] = "<keybind>",
-					["v_actionicon"] = "115069",
-				},
-				["defaultRotation/monk/blackout_kick"] = {
-					["v_p_needbuff"] = "115307^2",
-					["b_p_needbuff"] = true,
-					["b_p_unitpower"] = true,
-					["v_actionicon"] = "100784",
-					["v_togglename"] = "Toggle 1",
-					["v_p_unitpowertype"] = "12",
-					["v_spellname"] = "100784",
-					["v_durationstartedtime"] = 0,
-					["v_keybind"] = "<keybind>",
-					["v_p_unitpower"] = ">=2",
+					["v_p_unitpower"] = ">=65",
+					["v_gspellcost"] = "25",
 				},
 				["defaultRotation/monk/keg_smash"] = {
-					["v_actionicon"] = "121253",
-					["b_p_unitpower"] = true,
+					["b_gspellcost"] = true,
 					["v_togglename"] = "Toggle 1",
 					["v_durationstartedtime"] = 0,
 					["v_spellname"] = "121253",
-					["v_p_unitpowertype"] = "12",
+					["v_gspellcosttype"] = "3",
 					["v_keybind"] = "<keybind>",
-					["v_p_unitpower"] = "<=2",
-				},
-				["defaultRotation/monk/jab"] = {
-					["b_p_unitpower"] = true,
-					["v_gunitpowertype"] = "12",
-					["v_p_unitpower"] = ">=80",
-					["v_actionicon"] = "108557",
-					["b_gunitpower"] = true,
-					["v_togglename"] = "Toggle 1",
-					["v_p_unitpowertype"] = "3",
-					["v_spellname"] = "108557",
-					["v_durationstartedtime"] = 0,
-					["v_keybind"] = "<keybind>",
-					["v_gunitpower"] = "1",
-				},
-				["defaultRotation/monk/tiger_palm_refresh"] = {
-					["v_p_needbuff"] = "_125359^3",
-					["b_p_needbuff"] = true,
-					["v_togglename"] = "Toggle 1",
-					["v_durationstartedtime"] = 0,
-					["v_spellname"] = "100787",
-					["v_keybind"] = "<keybind>",
-					["v_actionicon"] = "100787",
-				},
-				["defaultRotation/monk/chi_wave"] = {
-					["v_p_knowspell"] = "115098",
-					["v_actionicon"] = "115098",
-					["b_p_knowspell"] = true,
-					["v_p_hp"] = "<=90%",
-					["v_durationstartedtime"] = 0,
-					["v_spellname"] = "115098",
-					["v_keybind"] = "<keybind>",
-					["b_p_hp"] = true,
-					["v_togglename"] = "Toggle 1",
+					["v_gspellcost"] = "40",
 				},
 			},
 			["SortedActions"] = {
-				"defaultRotation/monk/stance_of_the_sturdy_ox", -- [1]
-				"defaultRotation/monk/legacy_of_the_white_tiger", -- [2]
-				"defaultRotation/monk/summon_black_ox_statue", -- [3]
-				"defaultRotation/monk/guard", -- [4]
-				"defaultRotation/monk/blackout_kick", -- [5]
-				"defaultRotation/monk/tiger_palm_refresh", -- [6]
-				"defaultRotation/monk/zen_sphere", -- [7]
-				"defaultRotation/monk/chi_wave", -- [8]
-				"defaultRotation/monk/keg_smash", -- [9]
-				"defaultRotation/monk/expel_harm", -- [10]
-				"defaultRotation/monk/jab", -- [11]
-				"defaultRotation/monk/tiger_palm", -- [12]
+				"defaultRotation/monk/keg_smash", -- [1]
+				"defaultRotation/monk/tiger_palm", -- [2]
+				"defaultRotation/monk/blackout_strike", -- [3]
+				"defaultRotation/monk/breath_of_fire", -- [4]
 			},
-			["keybind"] = "<keybind>",
 		},
 	}
 
